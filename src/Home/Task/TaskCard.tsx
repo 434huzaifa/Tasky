b9e4d02f0320761fbe5a19531a0b4c26f56cf306
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2024-03-31 13:38:15 +0600

    task add form added

--- src/Home/Task/TaskCard.tsx
+++ src/Home/Task/TaskCard.tsx
@@ -1,15 +1,19 @@
-import { Card } from "antd";
+import { Card, Tooltip } from "antd";
 import { MdOutlineDeleteOutline } from "react-icons/md";
 import { TbClockDown } from "react-icons/tb";
 import { FaPen } from "react-icons/fa";
-import { GrInProgress } from "react-icons/gr";
-type Props={
-    title?:string
-    description?:string
-    type?:"todo"|"in-progress"|"completed"
-}
+import { TbClockCheck } from "react-icons/tb";
+type Props = {
+  title?: string;
+  description?: string;
+  type?: "todo" | "in-progress" | "completed";
+};
 
-const TaskCard = ({title="Title here",description="This is description...",type="todo"}:Props) => {
+const TaskCard = ({
+  title = "Title here",
+  description = "This is description...",
+  type = "todo",
+}: Props) => {
   return (
     <Card className="font-roboto-slab">
       <div className="flex items-center">
@@ -17,20 +21,29 @@ const TaskCard = ({title="Title here",description="This is description...",type=
           <p className="text-lg  font-bold">{title}</p>
           <p className="font-semibold">{description}</p>
         </div>
-        <div className=" px-4 hover:cursor-pointer">
-          <FaPen className="text-2xl text-center text-yellow-500" />
-        </div>
-        {
-            type=="todo"? <div className="border-l px-4 hover:cursor-pointer">
-            <TbClockDown className="text-3xl text-center text-blue-500" />
-          </div>: type=="in-progress"? <div className="border-l px-4">
-          <GrInProgress className="text-2xl text-center text-blue-500" />
-        </div>:null
-        }
-        
-        <div className="border-l px-4 hover:cursor-pointer">
-          <MdOutlineDeleteOutline className="text-3xl text-center text-red-500" />
-        </div>
+        <Tooltip title="Edit">
+          <div className=" px-4 hover:cursor-pointer">
+            <FaPen className="text-2xl text-center text-yellow-500" />
+          </div>
+        </Tooltip>
+        {type == "todo" ? (
+          <Tooltip title="Move to in progress">
+            {" "}
+            <div className="border-l px-4 hover:cursor-pointer">
+              <TbClockDown className="text-3xl text-center text-blue-500" />
+            </div>
+          </Tooltip>
+        ) : type == "in-progress" ? (
+          <Tooltip title="Move to complete">
+          <div className="border-l px-4">
+            <TbClockCheck className="text-2xl text-center text-blue-500 hover:cursor-pointer" />
+          </div></Tooltip>
+        ) : null}
+        <Tooltip title="Delete">
+          <div className="border-l px-4 hover:cursor-pointer">
+            <MdOutlineDeleteOutline className="text-3xl text-center text-red-500" />
+          </div>
+        </Tooltip>
       </div>
     </Card>
   );
--- src/Home/Task/TaskForm.tsx
+++ src/Home/Task/TaskForm.tsx
@@ -1,11 +1,37 @@
-import { Card } from "antd";
+import { Button, Card, Form, Input } from "antd";
+
+type FormDataType = {
+  title: string;
+  description?: string;
+};
 
 const TaskForm = () => {
-    return (
-        <Card>
-            
-        </Card>
-    );
+  function onFinish(values: FormDataType) {
+    console.log(values);
+  }
+  return (
+    <Card>
+      <Form layout="vertical" onFinish={onFinish}>
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[
+            { required: true },
+            { min: 5, message: "At least 5 letter." },
+          ]}
+        >
+          <Input></Input>
+        </Form.Item>
+        <Form.Item name="description" label="Description">
+          <Input.TextArea></Input.TextArea>
+        </Form.Item>
+
+        <div className="flex justify-center">
+          <Button htmlType="submit">Add</Button>
+        </div>
+      </Form>
+    </Card>
+  );
 };
 
 export default TaskForm;
