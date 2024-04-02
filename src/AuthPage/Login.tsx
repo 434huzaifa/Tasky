4c5a7def05022e5e10d415b4b42a11a9b98889c9
Md. Huzaifa <saadhuzaifa2497@gmail.com>
2024-04-02 10:22:22 +0600

    Register with emaill added

--- src/AuthPage/Login.tsx
+++ src/AuthPage/Login.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Button, Card, Divider, Form, Input } from "antd";
+import { Button, Card, Divider, Form, Input, Spin } from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import "./wavy.css";
@@ -13,7 +13,8 @@ import ErrorResponse from "../Utility/ErrorResponse";
 import { AxiosError } from "axios";
 const Login = () => {
   const navigate = useNavigate();
-  const { signIn, googleSignIn, changeLoading,assignJWT,logOut } = useAuth();
+  const { signIn, googleSignIn, loading, changeLoading, assignJWT, logOut } =
+    useAuth();
   const caxios = useAxios();
   const mutationUserCreateOrCheck = useMutation({
     mutationFn: async (data: { email: string; name: string }) => {
@@ -23,10 +24,10 @@ const Login = () => {
     onSuccess: (data) => {
       changeLoading(false);
       SuccessResponse(data);
-      navigate("/");      
+      navigate("/");
     },
     onError: (err: AxiosError) => {
-      changeLoading(false)
+      changeLoading(false);
       ErrorResponse(err);
     },
   });
@@ -38,24 +39,24 @@ const Login = () => {
             email: res.user.email,
             name: res.user.displayName,
           });
-          assignJWT(res.user.email).then(()=>{
-            changeLoading(false);
-          }).catch(async (err:any) => {
-            await logOut();
-            changeLoading(false);
-            console.log("assignJWT~", err);
-            console.log(err.message);
-            navigate("/login")
-          })
+          assignJWT(res.user.email)
+            .then(() => {
+              changeLoading(false);
+            })
+            .catch(async (err: any) => {
+              await logOut();
+              changeLoading(false);
+              console.log("assignJWT~", err);
+              console.log(err.message);
+              navigate("/login");
+            });
         }
-        
       })
       .catch((err: any) => {
         console.log(err);
-        changeLoading(false)
+        changeLoading(false);
         showToast("error", err.message);
       });
-      
   }
   function handelGoogleLogin() {
     googleSignIn()
@@ -65,14 +66,16 @@ const Login = () => {
             email: res.user.email,
             name: res.user.displayName,
           });
-          assignJWT(res.user.email).then(()=>{
-            changeLoading(false);
-          }).catch(async (err:any) => {
-            await logOut();
-            changeLoading(false);
-            console.log("assignJWT~", err);
-            navigate("/login")
-          })
+          assignJWT(res.user.email)
+            .then(() => {
+              changeLoading(false);
+            })
+            .catch(async (err: any) => {
+              await logOut();
+              changeLoading(false);
+              console.log("assignJWT~", err);
+              navigate("/login");
+            });
         }
       })
       .catch((err: any) => {
@@ -80,76 +83,77 @@ const Login = () => {
         changeLoading(false);
         showToast("error", err.message);
       });
-      
   }
   return (
     <div className=" justify-center flex items-center min-h-screen bg-wavy">
-      <Card className="w-96 pb-12 pt-6 backdrop-blur-md bg-opacity-0 bg-white">
-        <div>
-          <p className="mb-5 text-center text-3xl font-roboto-slab text-black">
-            <span className="text-purple-800">Welcome,</span> To Tasky
-          </p>
-          <Form layout="vertical" onFinish={onFinish}>
-            <Form.Item
-              name="email"
-              label="Email"
-              className="font-bold"
-              rules={[
-                { required: true },
-                {
-                  type: "email",
-                  message: "The input is not valid E-mail!",
-                },
-              ]}
-              validateTrigger="onBlur"
-            >
-              <Input className="font-semibold font-roboto-slab"></Input>
-            </Form.Item>
-            <Form.Item
-              name="pass"
-              label="Password"
-              className="font-bold"
-              rules={[
-                { required: true },
-                {
-                  min: 6,
-                  message: "Password Less then Six.",
-                },
-                {
-                  pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
-                  message:
-                    "At least one uppercase,one lowercase and one number.",
-                },
-              ]}
-              validateTrigger="onBlur"
-            >
-              <Input.Password className="font-semibold font-roboto-slab"></Input.Password>
-            </Form.Item>
-            <div className="flex justify-center items-center flex-col">
-              <Button
-                size="large"
-                htmlType="submit"
-                className="bg-orange-300 text-black font-semibold font-roboto-slab w-1/2"
+      <Spin spinning={loading}>
+        <Card className="w-96 pb-12 pt-6 backdrop-blur-md bg-opacity-0 bg-white">
+          <div>
+            <p className="mb-5 text-center text-3xl font-roboto-slab text-black">
+              <span className="text-purple-800">Welcome,</span> To Tasky
+            </p>
+            <Form layout="vertical" onFinish={onFinish}>
+              <Form.Item
+                name="email"
+                label="Email"
+                className="font-bold"
+                rules={[
+                  { required: true },
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!",
+                  },
+                ]}
+                validateTrigger="onBlur"
               >
-                Login
-              </Button>
-              <Divider>OR</Divider>
-              <div className="my-2">
-                <Button size="large" onClick={handelGoogleLogin}>
-                  <FcGoogle className="text-2xl" />
+                <Input className="font-semibold font-roboto-slab"></Input>
+              </Form.Item>
+              <Form.Item
+                name="pass"
+                label="Password"
+                className="font-bold"
+                rules={[
+                  { required: true },
+                  {
+                    min: 6,
+                    message: "Password Less then Six.",
+                  },
+                  {
+                    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+                    message:
+                      "At least one uppercase,one lowercase and one number.",
+                  },
+                ]}
+                validateTrigger="onBlur"
+              >
+                <Input.Password className="font-semibold font-roboto-slab"></Input.Password>
+              </Form.Item>
+              <div className="flex justify-center items-center flex-col">
+                <Button
+                  size="large"
+                  htmlType="submit"
+                  className="bg-orange-300 text-black font-semibold font-roboto-slab w-1/2"
+                >
+                  Login
                 </Button>
+                <Divider>OR</Divider>
+                <div className="my-2">
+                  <Button size="large" onClick={handelGoogleLogin}>
+                    <FcGoogle className="text-2xl" />
+                  </Button>
+                </div>
+                <Divider>OR</Divider>
+                <p className="text-lg font-semibold font-roboto-slab">
+                  If you don't have an account.{" "}
+                  <Link to="/reg" className="text-blue-500 underline">
+                    Sign Up
+                  </Link>
+                </p>
               </div>
-              <Divider>OR</Divider>
-              <p className="text-lg font-semibold font-roboto-slab">
-                If you don't have an account.{" "}
-                <Link to="/reg" className="text-blue-500 underline">
-                  Sign Up
-                </Link>
-              </p>
-            </div>
-          </Form>
-        </div>
-      </Card>
+            </Form>
+          </div>
+        </Card>
+      </Spin>
     </div>
   );
 };
--- src/AuthPage/Register.tsx
+++ src/AuthPage/Register.tsx
@@ -1,96 +1,111 @@
-import { Button, Card, Divider, Form, Input } from "antd";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Button, Card, Divider, Form, Input, Spin } from "antd";
 import { Link } from "react-router-dom";
-import { FcGoogle } from "react-icons/fc";
-import "./wavy.css"
+import "./wavy.css";
+import showToast from "../Utility/showToast";
+import useAuth from "../Hook/useAuth";
 const Register = () => {
+  const { createUser, changeLoading, loading } = useAuth();
+  function onFinish(values: any) {
+    if (values.pass1 == values.pass2) {
+      createUser(values.email, values.pass1)
+        .then(() => {
+          changeLoading(false);
+        })
+        .catch((err: any) => {
+          changeLoading(false);
+          showToast("error", err.message);
+          console.log(err);
+        });
+    } else {
+      showToast("error", "Password Mismatch");
+    }
+  }
 
-    return (
-        <div className=" justify-center flex items-center min-h-screen bg-wavy2">
-      <Card className="w-96 pb-12 pt-6 backdrop-blur-md bg-opacity-0 bg-white">
-        <div>
-          <p className="mb-5 text-center text-3xl font-roboto-slab text-black">
-            <span className="text-purple-800">Welcome,</span> To Tasky
-          </p>
-          <Form layout="vertical">
-          <Form.Item
-              name="name"
-              label="Name"
-              className="font-bold"
-              rules={[
-                { required: true },
-                
-              ]}
-              validateTrigger="onBlur"
-            >
-              <Input className="font-semibold font-roboto-slab"></Input>
-            </Form.Item>
-            <Form.Item
-              name="email"
-              label="Email"
-              className="font-bold"
-              rules={[
-                { required: true },
-                {
-                  type: "email",
-                  message: "The input is not valid E-mail!",
-                },
-              ]}
-              validateTrigger="onBlur"
-            >
-              <Input className="font-semibold font-roboto-slab"></Input>
-            </Form.Item>
-            <Form.Item
-              name="pass1"
-              label="Password"
-              className="font-bold"
-              rules={[
-                { required: true },
-                {
-                    min:6,
-                    message:"Password Less then Six."
-                },
-                {
-                    pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
-                    message:"At least one uppercase,one lowercase and one number."
-                }
-              ]}
-              validateTrigger="onBlur"
-            >
-              <Input.Password className="font-semibold font-roboto-slab"></Input.Password>
-            </Form.Item>
-            <Form.Item
-              name="pass2"
-              label="Confirm Password"
-              className="font-bold"
-              rules={[
-                { required: true },
-                {
-                    min:6,
-                    message:"Password Less then Six."
-                },
-                {
-                    pattern:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
-                    message:"At least one uppercase,one lowercase and one number."
-                }
-              ]}
-              validateTrigger="onBlur"
-            >
-              <Input.Password className="font-semibold font-roboto-slab"></Input.Password>
-            </Form.Item>
-            <div className="flex justify-center items-center flex-col">
-            <Button size="large" htmlType="submit" className="bg-orange-300 text-black font-semibold font-roboto-slab w-1/2">Register</Button>
-            <Divider>OR</Divider>
-            <div className="my-2">
-                <Button size="large"><FcGoogle className="text-2xl"/></Button>
-            </div>
-            <Divider>OR</Divider>
-            <p className="text-lg font-semibold font-roboto-slab">If you have an account. <Link to="/login" className="text-blue-500 underline">Login</Link></p>
-            </div>
-          </Form>
-        </div>
-      </Card>
+  return (
+    <div className=" justify-center flex items-center min-h-screen bg-wavy2">
+      <Spin spinning={loading}>
+        <Card className="w-96 pb-12 pt-6 backdrop-blur-md bg-opacity-0 bg-white">
+          <div>
+            <p className="mb-5 text-center text-3xl font-roboto-slab text-black">
+              <span className="text-purple-800">Welcome,</span> To Tasky
+            </p>
+            <Form layout="vertical" onFinish={onFinish}>
+              <Form.Item
+                name="name"
+                label="Name"
+                className="font-bold"
+                rules={[{ required: true }]}
+                validateTrigger="onBlur"
+              >
+                <Input className="font-semibold font-roboto-slab"></Input>
+              </Form.Item>
+              <Form.Item
+                name="email"
+                label="Email"
+                className="font-bold"
+                rules={[
+                  { required: true },
+                  {
+                    type: "email",
+                    message: "The input is not valid E-mail!",
+                  },
+                ]}
+                validateTrigger="onBlur"
+              >
+                <Input className="font-semibold font-roboto-slab"></Input>
+              </Form.Item>
+              <Form.Item
+                name="pass1"
+                label="Password"
+                className="font-bold"
+                rules={[
+                  { required: true },
+                  {
+                    min: 6,
+                    message: "Password Less then Six.",
+                  },
+                  {
+                    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+                    message:
+                      "At least one uppercase,one lowercase and one number.",
+                  },
+                ]}
+                validateTrigger="onBlur"
+              >
+                <Input.Password className="font-semibold font-roboto-slab"></Input.Password>
+              </Form.Item>
+              <Form.Item
+                name="pass2"
+                label="Confirm Password"
+                className="font-bold"
+                rules={[{ required: true }]}
+                validateTrigger="onBlur"
+              >
+                <Input.Password className="font-semibold font-roboto-slab"></Input.Password>
+              </Form.Item>
+              <div className="flex justify-center items-center flex-col">
+                <Button
+                  size="large"
+                  htmlType="submit"
+                  className="bg-orange-300 text-black font-semibold font-roboto-slab w-1/2"
+                >
+                  Register
+                </Button>
+                <Divider>OR</Divider>
+                <p className="text-lg font-semibold font-roboto-slab">
+                  If you have an account.{" "}
+                  <Link to="/login" className="text-blue-500 underline">
+                    Login
+                  </Link>
+                </p>
+              </div>
+            </Form>
+          </div>
+        </Card>
+      </Spin>
     </div>
-    );
+  );
 };
 
 export default Register;
